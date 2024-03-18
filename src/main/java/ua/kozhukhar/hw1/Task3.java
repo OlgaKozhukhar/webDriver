73d87b743db8814502b3f5befd20f5ec70f3dda2
OlgaKozhukhar <soi198607@gmail.com>
2024-03-18 21:56:32 +0200

    init comit

--- src/main/java/ua/kozhukhar/hw1/Task3.java
+++ src/main/java/ua/kozhukhar/hw1/Task3.java
@@ -1,23 +1,23 @@
 package ua.kozhukhar.hw1;
 
-import io.qameta.allure.internal.shadowed.jackson.databind.ser.Serializers;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+
 import java.time.Duration;
 import java.util.List;
 
-
-public class Task3 extends BaseTest {
+public class Task3 {
 
     @Test
     public void retrieveSearchResultData() {
         String searchValue = "тумба";
-        WebDriver webDriver;
+        WebDriver webDriver = new ChromeDriver(); // Инициализация WebDriver
         webDriver.get("https://prozorro.gov.ua/en");
         WebElement input = webDriver.findElement(By.xpath("//input[@class='search-text__input']"));
         input.clear();
@@ -31,13 +31,14 @@ public class Task3 extends BaseTest {
         WebElement costElement = webDriver.findElement(By.xpath("//strong[contains(text(),'98 940,00')]/span[@class='small']"));
         WebElement statusElement = webDriver.findElement(By.xpath("//span[@class='marked'][contains(text(),'Завершена')]"));
         WebElement companyElement = webDriver.findElement(By.xpath("//td[@class='col-sm-6' and contains(text(), 'Комунальне некомерційне підприємство Білоцерківської міської ради \"Білоцерківська міська лікарня №2\"')]"));
-        String name = nameElement.getText(); String tender  = "Меблі (тумба мобільна, тумба, тумба приставна)";
-        String cost = "98 940,00 UAH";
-        String status = "Завершена";
-        String company = "Комунальне некомерційне підприємство Білоцерківської міської ради \"Білоцерківська міська лікарня №2\" • Біла Церква";
+        String name = nameElement.getText();
+        String cost = costElement.getText(); // Исправление для получения текста стоимости
+        String status = statusElement.getText(); // Исправление для получения текста статуса
+        String company = companyElement.getText(); // Исправление для получения текста компании
         System.out.println("Название: " + name);
         System.out.println("Стоимость: " + cost);
         System.out.println("Статус: " + status);
         System.out.println("Компания: " + company);
+        webDriver.quit(); // Закрытие сессии WebDriver
     }
 }
